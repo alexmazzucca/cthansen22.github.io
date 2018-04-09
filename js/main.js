7636f7f0461920953d04c4796732a4a0354f12c2
Alex Mazzucca <alexmazzucca@archerconsulting.net>
2018-04-09 11:02:22 -0400

    Merge remote-tracking branch 'cthansen22/master' into home-video

--- js/main.js
+++ js/main.js
@@ -1,16 +1,88 @@
 /*
+* >>========================================>
+* Sticky Navigation
+* >>========================================>
+*/
+
+var lastScrollTop = 0,
+    stickyNavEnabled = false;
+
+$(window).on('scroll resize', stickyNav).trigger('scroll');
+
+function stickyNav(){
+    var st = $(window).scrollTop();
+
+    if(st > $(window).height() / 2){
+
+        if(st < lastScrollTop) {
+            if(!stickyNavEnabled) {
+                openStickyNav();
+            }
+            
+        }else{
+            closeStickyNav();
+        }
+    }else{
+        closeStickyNav();
+    }
+
+    lastScrollTop = st;
+}
+
+function openStickyNav(){
+
+    stickyNavEnabled = true;
+
+    $('<div class="sticky-toolbar" />').appendTo('body');
+
+    $('.primary-nav, .site-logo, .burger').clone().appendTo('.sticky-toolbar');
+
+    $(window).off('scroll resize', stickyNav)
+    
+    TweenMax.fromTo($('.sticky-toolbar'), .3, {y: -$('.sticky-toolbar').outerHeight()}, {y:0, ease:Power1.easeInOut, onComplete:function(){
+        $(window).on('scroll resize', stickyNav)
+    }})
+}
+
+function closeStickyNav(){
+    
+    stickyNavEnabled = false;
+
+    $(window).off('scroll resize', stickyNav);
+
+    TweenMax.fromTo($('.sticky-toolbar'), .3, {y:0}, {y: -$('.sticky-toolbar').outerHeight(), ease:Power1.easeInOut, onComplete:function(){
+        $('.sticky-toolbar').remove();
+        $(window).on('scroll resize', stickyNav)
+    }})
+}
+
+/*
 >>================================================================================>
 Mobile Menu
 >>================================================================================>
 */
 
-$('#burger').on('click', function(){
+$('body').on('click', '.burger', function(){
     if($('body').hasClass('mobile-menu-active')){
         $('body').removeClass('mobile-menu-active');
     }else{
         $('body').addClass('mobile-menu-active');
     }
 });
+
+/*
+>>================================================================================>
+Team
+>>================================================================================>
+*/
+
+$('#team .member').each(function(){
+    var $member = $(this);
+    $(this).find('.photo').add($(this).find('button.close')).on('click', function(){
+        $member.toggleClass('active');
+        $('body').toggleClass('disabled')
+    })
+})
 
 /*
 >>================================================================================>
@@ -31,6 +103,9 @@ Loading
 */
 
 $(window).on('load', function(){
+    if(document.referrer.search("hypernetwork.io") != -1){
+        $('#loading').remove();
+    }
     if($('#loading').length){
         $('body').addClass('page-loaded');
         setTimeout(function(){
@@ -107,6 +182,9 @@ $('#form').validate({
         }
     },
     submitHandler: function(form) {
+        if ($('#form').hasClass('slackThanksForm') || $('#form').hasClass('telegramThanksForm')) {
+            return true;
+        }
         $('#form').addClass('sending');
         $('#form button[type="submit"]').prop('disabled', true);
         $('.checkbox.error').removeClass('error');
@@ -120,7 +198,7 @@ $('#form').validate({
             contentType: "application/json; charset=utf-8",
             error: function(response) {
                 setTimeout(function(){
-                    $('#thanks').html("Could not connect to the registration server. Please try again later.");
+                    $('#output').html("Could not connect to the registration server. Please try again later.");
                     $('#form').addClass('success');
                     setTimeout(function(){
                         $('#form').removeClass('sending success');
@@ -134,9 +212,14 @@ $('#form').validate({
                     fbq('track', 'CompleteRegistration', {
                         content_name: 'Email Submission'
                     });
-                    $('#thanks').html("Thank you. We will be in contact shortly.");
+                    if ($('#form').hasClass('slackForm')) {
+                        window.location.href = "slack_thanks.html";
+                    } else if ($('#form').hasClass('telegramForm')) {
+                        window.location.href = "telegram_thanks.html";
+                    }
+                    $('#output').html("You're in! Welcome to Hypernet. We will be in contact shortly.");
                 } else {
-                    $('#thanks').html(response.msg);
+                    $('#output').html(response.msg);
                 }
                 setTimeout(function(){
                     $('#form').addClass('success');
@@ -150,3 +233,31 @@ $('#form').validate({
         });
     }
 });
+
+/*
+>>================================================================================>
+Social Nav Lock-In
+>>================================================================================>
+*/
+
+var viewportWidth;
+
+$(window).on('load resize', function(){
+    viewportWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+}).trigger('resize');
+
+$(window).on('scroll resize', function(){
+    if(viewportWidth > 767){
+
+        if($(window).scrollTop() >= $('#footer').offset().top - $(window).height()){
+            $('#menu nav.social').css({
+                position: 'absolute',
+                top: $('#footer').offset().top - ($(window).height()/2)
+            })
+        }else{
+            $('#menu nav.social').attr('style', '');
+        }
+    }else{
+        $('#menu nav.social').attr('style', '');
+    }
+})
