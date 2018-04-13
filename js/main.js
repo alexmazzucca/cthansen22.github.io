ea1c6236239391adf2afbc03623c58fc2cb62a65
Alex Mazzucca <alexmazzucca@archerconsulting.net>
2018-04-13 10:19:45 -0400

    Font paths updated, client approved

--- js/main.js
+++ js/main.js
@@ -4,52 +4,47 @@
 * >>========================================>
 */
 
-var lastScrollTop = 0,
-    stickyNavEnabled = false;
+if(!$('#front-page').length){
+    var lastScrollTop = 0,
+        stickyNavEnabled = false;
 
-$(window).on('scroll resize', stickyNav).trigger('scroll');
+    $(window).on('scroll resize', stickyNav).trigger('scroll');
 
-function stickyNav(){
-    var st = $(window).scrollTop();
+    function stickyNav(){
+        var st = $(window).scrollTop();
 
-    if(st > $(window).height() / 2){
+        if(st > $(window).height() / 2){
 
-        if(st < lastScrollTop) {
-            if(!stickyNavEnabled) {
-                openStickyNav();
+            if(st < lastScrollTop) {
+                if(!stickyNavEnabled) {
+                    openStickyNav();
+                }
+                
+            }else{
+                closeStickyNav();
             }
-            
         }else{
             closeStickyNav();
         }
-    }else{
-        closeStickyNav();
-    }
 
-    lastScrollTop = st;
+        lastScrollTop = st;
+    }
 }
 
+
 function openStickyNav(){
-
     stickyNavEnabled = true;
-
     $('<div class="sticky-toolbar" />').appendTo('body');
-
     $('.primary-nav, .site-logo, .burger').clone().appendTo('.sticky-toolbar');
-
     $(window).off('scroll resize', stickyNav)
-    
     TweenMax.fromTo($('.sticky-toolbar'), .3, {y: -$('.sticky-toolbar').outerHeight()}, {y:0, ease:Power1.easeInOut, onComplete:function(){
         $(window).on('scroll resize', stickyNav)
     }})
 }
 
 function closeStickyNav(){
-    
     stickyNavEnabled = false;
-
     $(window).off('scroll resize', stickyNav);
-
     TweenMax.fromTo($('.sticky-toolbar'), .3, {y:0}, {y: -$('.sticky-toolbar').outerHeight(), ease:Power1.easeInOut, onComplete:function(){
         $('.sticky-toolbar').remove();
         $(window).on('scroll resize', stickyNav)
@@ -80,7 +75,7 @@ $('#team .member').each(function(){
     var $member = $(this);
     $(this).find('.photo').add($(this).find('button.close')).on('click', function(){
         $member.toggleClass('active');
-        $('body').toggleClass('disabled')
+        $('body').toggleClass('disabled');
     })
 })
 
@@ -103,9 +98,10 @@ Loading
 */
 
 $(window).on('load', function(){
-    if(document.referrer.search("hypernetwork.io") != -1){
+    if(document.referrer.search("hypernetwork.io") != -1 || location.host == 'alexanders-imac.local:5757') {
         $('#loading').remove();
     }
+
     if($('#loading').length){
         $('body').addClass('page-loaded');
         setTimeout(function(){
@@ -132,8 +128,6 @@ $(window).on('load', function(){
         }, 0);
     }
 });
-
-//$('#loading').remove();
 
 /*
 >>================================================================================>
@@ -246,18 +240,39 @@ $(window).on('load resize', function(){
     viewportWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 }).trigger('resize');
 
-$(window).on('scroll resize', function(){
-    if(viewportWidth > 767){
-
-        if($(window).scrollTop() >= $('#footer').offset().top - $(window).height()){
-            $('#menu nav.social').css({
-                position: 'absolute',
-                top: $('#footer').offset().top - ($(window).height()/2)
-            })
+if($('#footer').length){
+    $(window).on('scroll resize', function(){
+        if(viewportWidth > 767){
+            if($(window).scrollTop() >= $('#footer').offset().top - $(window).height()){
+                $('#menu nav.social').css({
+                    position: 'absolute',
+                    top: $('#footer').offset().top - ($(window).height()/2)
+                })
+            }else{
+                $('#menu nav.social').attr('style', '');
+            }
         }else{
             $('#menu nav.social').attr('style', '');
         }
+    })
+}
+
+/*
+>>================================================================================>
+Intro Video
+>>================================================================================>
+*/
+
+$('.video-expand-toggle').on('click', function(){
+    $('body').toggleClass('video-expanded');
+
+    var $video = $('#video-modal video').get(0);
+
+    if($('body').hasClass('video-expanded')){
+        $video.play();
     }else{
-        $('#menu nav.social').attr('style', '');
+        if(!$video.paused) {
+            $video.pause();
+        }
     }
 })
